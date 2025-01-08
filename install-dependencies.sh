7a847611763c9ffd50e4b2d936ed77b636731d69
ant <aneubert@mobol.net>
2025-01-08 23:21:28 +0100

    added line for direnv to work

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -6,3 +6,5 @@ sudo apt update && \
   sudo apt update && \
   sudo apt-get install -y apt-transport-https ca-certificates gnupg && \
   sudo apt install -y git tree fonts-powerline fonts-firacode curl wget
+
+echo "eval \"\$(direnv hook zsh)\"" >> ~/.zshrc
